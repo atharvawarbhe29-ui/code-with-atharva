cda248ef8630c6691cd9459d54dbb97d33885662
Atharva Warbhe <atharvawarbhe29@gmail.com>
2026-08-17 06:27:53 +0530

    Create multiply.c

---
+++ C-Programming/operator/multiply.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+int maain(){
+  int num1 , num2 , multiply;
+printf("enter a number: ");
+scanf("%d", &num1);
+
+printf(enter another number: ");
+  scanf("%d", &num2);
+
+multiply = num1*num2);
+printf( multiplication of above two number is :%d\n",multiply);
+return 0 ;
+}
