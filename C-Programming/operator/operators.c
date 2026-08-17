ca011e7005a95eae2ca1559d089eccf700a9d074
Atharva Warbhe <atharvawarbhe29@gmail.com>
2026-08-17 06:42:03 +0530

    Create operators.c

---
+++ C-Programming/operator/operators.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+int main(){
+  int a = 25, b = 5;
+printf ("addition: %d\n", a+b);
+printf("subtraction: %d\n", a - b);
+printf("multiplication: %d\n", a*b);
+printf(""is a>b? %d\n", a>b);
+return 0;
+}
