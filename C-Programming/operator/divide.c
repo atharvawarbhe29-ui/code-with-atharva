8e106ec704dbb0dcaeef1a9d284a74441be48f3e
Atharva Warbhe <atharvawarbhe29@gmail.com>
2026-08-17 06:15:56 +0530

    Add divide function in divide.c for integer division

---
+++ C-Programming/operator/divide.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+int main (){
+  int num1 , num2 , division;
+
+printf('enter freist number: ");
+  scanf("%d", &num1);
+
+printf("enter second number (denominator shouldn't be zero): ");
+scanf("%d", &num2);
+
+division = num1/num2;
+
+printf("the division of obove twonumber is: %d\n", division);
+return 0;
+
+}
