10dfa7faf5cfb2a93ba9abacb6d058d576d770b3
Atharva Warbhe <atharvawarbhe29@gmail.com>
2026-08-29 19:01:26 +0530

    Create task-subtract.c

---
+++ C-Programming/operator/task-subtract.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+int main(){
+  int num1 , num2 , difference;
+
+printf("enter frist number: ");
+scanf("%d", &num1);
+
+printf("enter second number: ");
+scanf("%d", &num2);
+difference = num1 - num2 ;
+
+printf("difference of your entered number is: %d\n", difference);
+return 0;
+}
